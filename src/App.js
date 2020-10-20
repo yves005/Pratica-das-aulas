41f44ea3ea75a4cd8a083a91c67c073b319e7a93
yves005 <yvesabrahaols@gmail.com>
2020-10-20 11:43:21 -0300

    sei la

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Routes from "./routes"
+import "./global.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
